e77722e779dd6439be6dbd4c9af80e1b26feb01c
gabcdr <gabriel.coudurier@edu.ece.fr>
2023-04-23 16:27:05 +0200

    field name

--- Client/src/Administrateur.java
+++ Client/src/Administrateur.java
@@ -1,19 +1,37 @@
 import javax.swing.*;
 
 public class Administrateur {
-    private JButton STATSButton;
     private JButton BANNIRButton;
-    private JButton MODOButton;
-    private JButton ADMINButton;
-    private JLabel PSEUDO0;
-    private JLabel PSEUDO1;
-    private JLabel PSEUDO2;
-    private JLabel PSEUDO3;
-    private JLabel PSEUDO4;
-    private JLabel PSEUDO5;
-    private JLabel PSEUDO6;
-    private JLabel PSEUDO7;
-    private JLabel PSEUDO8;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton PROMOTIONButton;
+    private JButton button17;
+    private JButton button18;
+    private JButton button19;
+    private JButton button20;
+    private JButton button21;
+    private JButton button22;
+    private JButton button23;
+    private JButton button24;
+    private JButton button25;
+    private JButton button26;
+    private JButton button27;
+    private JButton button28;
+    private JButton button29;
+    private JButton button30;
+    private JButton STATISTIQUESButton;
 
 
     public Administrateur(){
